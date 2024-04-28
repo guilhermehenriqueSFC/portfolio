8b710dfb3e2514b9e2632fd44534176a6e90f92f
guilhermehenriqueSFC <165093826+guilhermehenriqueSFC@users.noreply.github.com>
2024-04-28 06:52:07 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,15 +3,33 @@ let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
 
-btnMenu.addEventListener('click', ()=>{
+btnMenu.addEventListener('click', () => {
     menu.classList.add('abrir-menu')
 })
 
-menu.addEventListener('click', ()=>{
+menu.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
 })
 
-overlay.addEventListener('click', ()=>{
+overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
 })
+document.addEventListener("DOMContentLoaded", function () {
+    const linksMenu = document.querySelectorAll('header nav a');
 
+    linksMenu.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            const offsetTop = targetElement.offsetTop;
+
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
